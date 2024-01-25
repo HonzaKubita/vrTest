44b9729d7e537c01e1e2fd8cbfc77951cdfe582a
Honza Kubita <honza.kubita@gmail.com>
2024-01-25 21:21:01 +0100

    Added ball object

---
+++ modules/world/objects/ball.js
@@ -0,0 +1,33 @@
+import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import PhysObject from "../templates/physObject";
+
+export default class Ball extends PhysObject {
+    constructor(radius, staticObject=false) {
+        // Create CANNON object
+        const cannonBody = new CANNON.Body({
+            // Calculate mass based on size
+            mass: staticObject ? 0 : (radius * 1),
+            // Calculate half extents based on size
+            shape: new CANNON.Sphere(radius),
+        });
+
+        const textureLoader = new THREE.TextureLoader();
+
+        // Create three object
+        const geometry = new THREE.SphereGeometry(radius, 32, 32);
+        const material = new THREE.MeshStandardMaterial({
+            map: textureLoader.load("/assets/textures/ball.jpeg"),
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+
+        // Call super constructor
+        super(cannonBody, mesh);
+
+        this.isGrabbable = !staticObject;
+        this.grabDistance = radius;
+    }
+}
